bf051ad69f5b33325852cfab2c12eb191b0d6b60
Urjit Sharma <104162053+UrjitS@users.noreply.github.com>
2023-11-01 11:46:25 -0700

    Added template files

---
+++ Server/src/main.cpp
@@ -0,0 +1,11 @@
+#include "networking.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    // Get cmd line arguments
+//    struct networking_options networkingOptions{};
+
+    return EXIT_SUCCESS;
+}

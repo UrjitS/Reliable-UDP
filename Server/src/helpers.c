b5e84b0065c227599e2b060461b809ce0ed73300
Colin Lam <colin.lph.lam@gmail.com>
2023-12-08 12:17:17 -0800

    ignore

--- Server/src/helpers.c
+++ Server/src/helpers.c
@@ -7,6 +7,7 @@
 void write_to_graph(FILE *graph, uint32_t ack_num, time_t start_time)
 {
     time_t now = time(0);
+    now = start_time - now;
     fprintf(graph, "%d, %ld\n", ack_num, now);
 }
 void write_to_stat(FILE *stat, uint32_t server_seq_num, uint32_t client_seq_num)

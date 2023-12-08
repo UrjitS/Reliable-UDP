079dcdfeef863104adc2011a22c31d8e200fcad4
Colin Lam <colin.lph.lam@gmail.com>
2023-12-08 12:19:38 -0800

    ignore

--- Server/src/helpers.c
+++ Server/src/helpers.c
@@ -7,12 +7,14 @@
 void write_to_graph(FILE *graph, uint32_t ack_num, time_t start_time)
 {
     time_t now = time(0);
-    now = start_time - now;
+    now = now - start_time;
     fprintf(graph, "%d, %ld\n", ack_num, now);
+    fflush(graph);
 }
 void write_to_stat(FILE *stat, uint32_t server_seq_num, uint32_t client_seq_num)
 {
     fprintf(stat, "Number of Packets Received from Client: %d\n", client_seq_num);
     fprintf(stat, "Number of Packets Sent by Server: %d\n", server_seq_num);
+    fflush(stat);
 }
 
